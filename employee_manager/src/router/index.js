311001bf31de8e87439e8fe2256b99d622768019
umv23 <umv23@cs.drexel.edu>
2020-03-24 11:04:03 -0400

    added styling componentes and added routes

--- employee_manager/src/router/index.js
+++ employee_manager/src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+import Dashboard from '../views/Dashboard.vue';
+import EditEmployee from '../views/EditEmployee.vue';
+import ViewEmployee from '../views/ViewEmployee.vue';
+import NewEmployee from '../views/NewEmployee.vue';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+const routes = [{
+  path: '/',
+  name: 'dashboard',
+  component: Dashboard,
+},
+{
+  path: '/new',
+  name: 'newEmployee',
+  component: NewEmployee,
+},
+{
+  path: '/edit/:emp_id',
+  name: 'editEmployee',
+  component: EditEmployee,
+},
+{
+  path: '/:emp_id',
+  name: 'viewEmployee',
+  component: ViewEmployee,
+},
 ];
 
 const router = new VueRouter({
