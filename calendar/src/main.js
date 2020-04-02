ae3512260d9b13aafa2e85e43f2b15a9ac5c8c51
umv23 <umv23@cs.drexel.edu>
2020-04-02 12:08:06 -0400

    adding components and default packages

--- calendar/src/main.js
+++ calendar/src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import fConfig from './components/firebaseConfig';
+import VueTextareAutosize from 'vue-textarea-autosize';
+
+Vue.use(VueTextareAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp(fConfig);
+// eslint-disable-next-line no-unused-vars
+const db = firebase.firestore();
+
 new Vue({
-  vuetify,
-  render: h => h(App)
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
