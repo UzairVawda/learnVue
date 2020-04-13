718a4a7b638e4eb12972b5231513831485b3f3ec
umv23 <umv23@cs.drexel.edu>
2020-04-13 10:05:11 -0400

    To use firebase, Create an init and config file (more in readme)

--- employee_manager/src/components/firebaseInit.js
+++
@@ -1,9 +0,0 @@
-import firebaseConfig from './firebaseConfig';
-
-const firebase = require('firebase/app');
-require('firebase/firestore');
-require('firebase/auth');
-
-const db = firebase.initializeApp(firebaseConfig);
-
-export default db;
